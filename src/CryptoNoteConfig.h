dc95d1af33c58ffd07111c1159484a86fd647f23
bestchancecoin <45109769+bestchancecoin@users.noreply.github.com>
2018-11-21 20:04:13 +0300

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "bestchancecoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff00019da417029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121014d52a1edb4b8486731dfb0f390f79be6bb6ceaa106e0dabda11bf49ecccdc8a8";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
