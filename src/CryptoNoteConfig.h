6f9aeff2af503ae8d9166629fb5b55ccf408ece9
bestchancecoin <45109769+bestchancecoin@users.noreply.github.com>
2018-11-21 19:58:15 +0300

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,7 +106,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  "94.177.234.109:17236",
+  "94.177.243.188:17236",
   "94.177.243.188:17236",
 };
 
